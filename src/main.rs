b9a200b284ced7e8d968546abefeccc9594c02ef
J/A <archer884@gmail.com>
2021-01-16 01:42:43 -0600

    Fix documentation

--- src/main.rs
+++ src/main.rs
@@ -3,14 +3,19 @@ mod error;
 mod expression;
 
 use bounded_rng::BoundedRngProvider;
-use clap::{crate_authors, crate_description, crate_version, Clap};
+use clap::{crate_authors, crate_version, Clap};
 use error::Error;
 use expression::Expression;
 
 type Result<T, E = Error> = std::result::Result<T, E>;
 
+/// A dice roller.
+///
+/// Use expressions like 2d8+d6+5. Results are printed with the total first,
+/// followed by each individual roll. Max rolls (crits) are highlighted in green,
+/// while low rolls are highlighted in red.
 #[derive(Clap, Clone, Debug)]
-#[clap(author = crate_authors!(), version = crate_version!(), about = crate_description!())]
+#[clap(author = crate_authors!(), version = crate_version!())]
 struct Opts {
     expressions: Vec<Expression>,
 }
